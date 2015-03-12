c9e12bd119ea22b492db0d0735dd00e6f86cc2c5
Dieter Plaetinck <dieter@vimeo.com>
2015-03-12 16:33:48 -0400

    bump worker timeout, graphite can take a while...

--- graphite-api.sh
+++ graphite-api.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec /sbin/setuser root gunicorn -b 0.0.0.0:8000 -w 2 --log-level debug graphite_api.app:app >>/var/log/graphite-api.log 2>&1
+exec /sbin/setuser root gunicorn -b 0.0.0.0:8000 -w 2 --timeout 120 --log-level debug graphite_api.app:app >>/var/log/graphite-api.log 2>&1
